9a25a98600848b659a0fb6ad4e55f177b287754b
mikamts <mikaelajoymatias@gmail.com>
2025-03-31 16:24:35 -0500

    sample demo

---
+++ TestWPF/TestWPF/homepage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestWPF
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            testMessage.Content = "Going to page 1";
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            testMessage.Content = "Going to page 2";
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            testMessage.Content = "Going to page 3";
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            testMessage.Content = "Going to page 4";
+        }
+
+    }
+}
